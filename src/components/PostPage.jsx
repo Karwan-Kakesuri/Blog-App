7825c24a4775b99058f455d472f28ea95386da65
Karwan Kakesuri <karwankakesuri@gmail.com>
2025-04-23 18:10:04 +0330

    style post page, not complete

--- src/components/PostPage.jsx
+++ src/components/PostPage.jsx
@@ -5,8 +5,17 @@ const PostPage = ({ posts }) => {
   const post = posts.find((post) => post.id == id);
 
   return (
-    <article>
-      <p>{post.title}</p>
+    <article className="d-flex flex-column rounded-4 p-8 mb-32 mt-32">
+      <div className="mb-4">
+        <p className="f-color-mute f-size-body2">{post.title}</p>
+      </div>
+      <div className="mb-16">
+        <p className="f-color-secondary f-size-body1">{post.body}</p>
+      </div>
+      <div className="d-flex gap-16 bt-1-d-vlg p-4">
+        <p className="f-color-tertiary f-size-body3 post-cred">{`By: ${post.author}`}</p>
+        <p className="f-color-tertiary f-size-body3 post-cred">{post.date}</p>
+      </div>
     </article>
   );
 };
